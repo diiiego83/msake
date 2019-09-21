4e7081bb97e74ad0721bc04f1a622bea62dcf7e1
diiiego83 <moltediego@gmail.com>
2019-09-21 13:24:15 +0100

    scripts updated

--- msake.sh
+++ msake.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-MSAKE_ROOT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+MSAKE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 CMD=$1; shift;
 CMD="$(echo "$CMD" | tr '[:upper:]' '[:lower:]')"
 
 if [ "$CMD" == "create" ]; then
-    source $MSAKE_ROOT_DIR/tools/service-gen.sh
+    source $MSAKE_DIR/tools/service-gen.sh
     createMicroService $@
     exit 0
 fi
 
 if [ "$CMD" == "build" ]; then
-    source $MSAKE_ROOT_DIR/tools/service-build.sh
+    source $MSAKE_DIR/tools/service-build.sh
     buildMicroService $@
     exit 0
 fi
--- tools/service-gen.sh
+++ tools/service-gen.sh
@@ -7,7 +7,7 @@ function parseInputArgs() {
             shift
             ;;
             --service-name=*)
-            SVCNAME="${i#*=}"
+            SERVICE_NAME="${i#*=}"
             shift
             ;;            
             *) 
@@ -22,14 +22,14 @@ function parseInputArgs() {
         exit 1
     fi
 
-    if [ -z $SVCNAME ]; then
+    if [ -z $SERVICE_NAME ]; then
         printf "\n[msake-error] required input arguments --service-name=<name of the service>\n"
         exit 1
     fi
 
     # lowercase
     LANGUAGE="$(echo "$LANGUAGE" | tr '[:upper:]' '[:lower:]')"
-    SVCNAME="$(echo "$SVCNAME" | tr '[:upper:]' '[:lower:]')"
+    SERVICE_NAME="$(echo "$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')"
 
 }
 
@@ -37,14 +37,18 @@ function createMicroService() {
 
     parseInputArgs $@
 
-    # [todo] do no create if exist
-    mkdir $SVCNAME
-    cd $SVCNAME
+    if [ -d "$SERVICE_NAME" ]; then
+        printf "\n[msake-error] $SERVICE_NAME already exist in folder\n"
+        exit 1
+    fi
+    mkdir $SERVICE_NAME
+    cd $SERVICE_NAME
+    SERVICE_DIR=$(pwd)
 
     if [ "$LANGUAGE" == "node" ]; then
 
-        cp -r $MSAKE_ROOT_DIR/models/node/* .
-        cp $MSAKE_ROOT_DIR/models/node/.gitignore .
+        cp -r $MSAKE_DIR/models/node/* .
+        cp $MSAKE_DIR/models/node/.gitignore .
         mv package package.json
         mv README README.md
         mv tsconfig tsconfig.json
@@ -54,20 +58,31 @@ function createMicroService() {
         mv src/server/server src/server/server.ts
 
     elif [ "$LANGUAGE" == "go" ]; then
-        cp -r $MSAKE_ROOT_DIR/models/go/* .
-        cp $MSAKE_ROOT_DIR/models/go/.gitignore .
+        cp -r $MSAKE_DIR/models/go/* .
+        cp $MSAKE_DIR/models/go/.gitignore .
     elif [ "$LANGUAGE" == "python" ]; then
-        cp -r $MSAKE_ROOT_DIR/models/python/* .
-        cp $MSAKE_ROOT_DIR/models/python/.gitignore .
+        cp -r $MSAKE_DIR/models/python/* .
+        cp $MSAKE_DIR/models/python/.gitignore .
     else
         printf "\n[msake-error] the \"$LANGUAGE\" is not a supported language, --language=<node python go>\n"
         exit 1    
     fi
 
     # replace token service-name
-    find . -type f -exec sed -i "s/#{service-name}#/$SVCNAME/g" {} +
+    find . -type f -exec sed -i "s/#{service-name}#/$SERVICE_NAME/g" {} +
     
     # set title in README.md
-    echo "# $(tr '[:lower:]' '[:upper:]' <<< ${SVCNAME:0:1})${SVCNAME:1}"> README.md
+    echo "# $(tr '[:lower:]' '[:upper:]' <<< ${SERVICE_NAME:0:1})${SERVICE_NAME:1}"> README.md
+
+    # init git and submodule
+    git init
+    cd $MSAKE_DIR
+    MSAKE_GIT_URL_REMOTE="$(git config --get remote.origin.url)"
+    cd $SERVICE_DIR
+    git submodule add $MSAKE_GIT_URL_REMOTE
+
+    # create the first commit
+    git add .
+    git commit -m "msake initialization"
 
 }
